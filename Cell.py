2846e30644d99435d61bf4703a65beada5ec7bc3
ShushoB <114217901+ShushoB@users.noreply.github.com>
2023-02-11 23:00:11 +0400

    Update Cell.py

--- Cell.py
+++ Cell.py
@@ -1,6 +1,8 @@
 import datetime
 
+
 Color = {0: 'white', 1: 'red', 2: 'green', 3: 'blue'}
+
 
 class Cell:
     def __init__(self, value="", color=0):
@@ -18,21 +20,25 @@ class Cell:
 
     def set_color(self, color):
         self.color = Color.get(color)
-        return self.color
 
     def to_int(self):
-        self.value = int(self.value)
-        return self.value
+        try:
+            self.value = int(self.value)
+        except ValueError:
+            print("Cannot convert to int")
 
     def to_float(self):
-        self.value = float(self.value)
-        return float(self.value)
+        try:
+            self.value = float(self.value)
+        except ValueError:
+            print("Cannot convert to float")
 
     def to_date(self):
-        self.value = datetime.datetime.strptime(self.value, "%d/%m/%Y").date()
-        return self.value
+        try:
+            self.value = datetime.datetime.strptime(self.value, "%d/%m/%Y").date()
+        except ValueError:
+            print("Cannot convert to date")
 
     def reset(self):
         self.value = ''
         self.color = Color.get(0)
-
